276b5830c04e0162f23a97b3e02d6f13f248b68a
Godswill <onwusilikegodswill@gmail.com>
2025-01-30 09:34:55 +0100

    deployment changes

--- MyInfo/settings.py
+++ MyInfo/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-nct+r4kg#z#2sq)bilwffheqw@4o$b#nj6=mvv6j^^nu^12jub
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
