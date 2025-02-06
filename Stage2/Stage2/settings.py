2aba7c353b1e6cc3fb8a0d5ae79555be1c86f4dd
Godswill <onwusilikegodswill@gmail.com>
2025-02-06 11:26:44 +0100

    setup for deployment

--- Stage2/Stage2/settings.py
+++ Stage2/Stage2/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ygmv++1(enbie@c2k3_8%l!2l5_fmxtbbb_q!+!!xbbx+yp!&b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
